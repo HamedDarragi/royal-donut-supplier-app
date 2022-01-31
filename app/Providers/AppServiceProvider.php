fc069eede65eae1d739ab857b22c379787d1636a
Hamed Darragi <darragihamed@gmail.com>
2022-01-31 10:24:09 +0100

    Force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        \URL::forceScheme('https');
         // Shortcodes::add([
         //     'a' => AllShortcode::class,
         //     // 'b' => BShortcode::class,
